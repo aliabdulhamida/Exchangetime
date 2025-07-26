82fc96ecd0ccb2212da2dee2215843bafa50bbb5
aliabdulhamida <44941620+aliabdulhamida@users.noreply.github.com>
2025-07-26 05:05:35 +0200

    web app download overlay

--- components/exchangetime/layout.tsx
+++ components/exchangetime/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from "./sidebar"
 import TopNav from "./top-nav"
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
+import DownloadAppOverlay from "@/components/DownloadAppOverlay"
 
 interface LayoutProps {
   children: ReactNode;
@@ -24,14 +25,17 @@ export default function Layout({ children, sidebar }: LayoutProps) {
   }
 
   return (
-    <div className={`flex h-screen ${theme === "dark" ? "dark" : ""}`}>
-      {sidebar}
-      <div className="w-full flex flex-1 flex-col">
-        <header className="h-16 border-b border-gray-200 dark:border-[#1F1F23]">
-          <TopNav />
-        </header>
-        <main className="flex-1 overflow-auto p-6 bg-white dark:bg-[#0F0F12]">{children}</main>
+    <>
+      <DownloadAppOverlay />
+      <div className={`flex h-screen ${theme === "dark" ? "dark" : ""}`}>
+        {sidebar}
+        <div className="w-full flex flex-1 flex-col">
+          <header className="h-16 border-b border-gray-200 dark:border-[#1F1F23]">
+            <TopNav />
+          </header>
+          <main className="flex-1 overflow-auto p-6 bg-white dark:bg-[#0F0F12]">{children}</main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
