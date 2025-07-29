9044e7c255373218991db327496db21986ed8839
aliabdulhamida <44941620+aliabdulhamida@users.noreply.github.com>
2025-07-29 03:26:56 +0200

    ec-calendar mobile fix

--- components/stock-market/TradingviewEc-Calendar.jsx
+++ components/stock-market/TradingviewEc-Calendar.jsx
@@ -20,13 +20,17 @@ function TradingViewWidget() {
           "countryFilter": "ar,au,br,ca,cn,fr,de,in,id,it,jp,kr,mx,ru,sa,za,tr,gb,us,eu",
           "importanceFilter": "0,1",
           "width": "100%",
-          "height": "600px"
+          "height": "700px"
         }`;
     container.current.appendChild(script);
   }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: 600, minHeight: 600 }}>
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: 700, minHeight: 700, maxHeight: '99vh', width: '100%' }}
+    >
       <div className="tradingview-widget-container__widget"></div>
     </div>
   );
